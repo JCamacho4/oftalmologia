0015b6f3a10d1fa9b7181ff8310299ac8dff3fdf
C4m4ch0 <jorgecamachog@uma.es>
2022-12-07 11:52:06 +0100

    mas cosas de mierda

--- src/main/java/com/gestion/mavenproject/App.java
+++ src/main/java/com/gestion/mavenproject/App.java
@@ -1,13 +1,32 @@
 package com.gestion.mavenproject;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence; 
+
+import org.apache.openjpa.*;
+import org.apache.openjpa.conf.OpenJPAConfiguration;
+
+import com.mysql.jdbc.Driver;
+
+import model.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final String PERSISTENCE_UNIT_NAME = "infoevau";
+	private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+			.createEntityManagerFactory(PERSISTENCE_UNIT_NAME); 
+	
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	final EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+		final Sede person = entityManager.find(Sede.class, "Nº 01 Facultad de Medicina");
+		System.out.println(person);
+		entityManager.close();
+		ENTITY_MANAGER_FACTORY.close();
     }
 }
--- src/main/java/com/gestion/mavenproject/JPAConnection.java
+++
@@ -1,5 +0,0 @@
-package com.gestion.mavenproject;
-
-public class JPAConnection {
-
-}

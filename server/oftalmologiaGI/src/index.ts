8b05e864c337372b95d3673474ee3c5672e163c8
C4m4ch0 <jorgecamachog@uma.es>
2022-12-09 17:10:27 +0100

    .env, y cositas de endpoint

--- server/oftalmologiaGI/src/index.ts
+++ server/oftalmologiaGI/src/index.ts
@@ -2,18 +2,75 @@ import { AppDataSource } from "./data-source"
 import { tClient } from "./entity/tClient"
 import { tEye } from "./entity/tEye"
 
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+app.listen(3001, () => {
+	console.log("Server running on port 3001");
+});
+
+// repositorios
+const clientRepository = AppDataSource.getRepository(tClient);
+const revisionRepository = AppDataSource.getRepository(tEye);
+
+/* 	========================================================
+	Consultas de tablas
+	  ========================================================*/
+
+app.get("/lClientes", async (req, res) => {
+	let r = await clientRepository.find();
+	res.send(r);
+});
+
+app.post("/lRevisiones", async (req, res) => {
+	let clientNIF = req.body.CLIENT;
+	let r = await revisionRepository.findBy({
+		CLIENT: clientNIF,
+	})
+	res.send(r);
+});
+
+
+/* 	========================================================
+	Inserciones
+	  ========================================================*/
+
+app.post("/insCliente", async (req, res) => {
+	try {
+		const client = new tClient();
+		client.NIF = req.body.NIF;
+		client.NOMBRE = req.body.NOMBRE;
+		client.APELLIDOS = req.body.APELLIDOS;
+		client.EDAD = Number(req.body.EDAD);
+		await clientRepository.save(client)
+		res.send(client);	// si la inserción es correcta devuelvo el cliente.
+	} catch (err) {
+		res.status(500).send({ status: 'error', message: err.message });
+	}
+});
+
+app.post("/insCita", async (req, res) => {
+	try {
+		
+	} catch (err) {
+		res.status(500).send({ status: 'error', message: err.message });
+	}
+});
+
+
+
+
+
+
+/*
 AppDataSource.initialize().then(async () => {
 
 	const clientRepository = AppDataSource.getRepository(tClient)
 	const revisionRepository = AppDataSource.getRepository(tEye)
 
-	console.log("Vamos a probar cositas")
-	const client = new tClient()
-	client.NIF = "12345678A"
-	client.NOMBRE = "Jorge"
-	client.APELLIDOS = "Camacho el mas lindo"
-	client.EDAD = 3
-	await clientRepository.save(client)
 
 	const client2 = new tClient()
 	client2.NIF = "87654321Z"
@@ -21,9 +78,6 @@ AppDataSource.initialize().then(async () => {
 	client2.APELLIDOS = "Fascista Americano"
 	client2.EDAD = 200
 	await clientRepository.save(client2)
-
-	const clients = await clientRepository.find()
-	console.log("Cargando todos los clientes de la base de datos:\n", clients)
 
 	const revision = new tEye()
 	revision.CLIENT = client
@@ -47,3 +101,4 @@ AppDataSource.initialize().then(async () => {
 	console.log("Cargando todos los clientes de la base de datos con sus relaciones:\n", clients2)
 
 }).catch(error => console.log(error))
+*/

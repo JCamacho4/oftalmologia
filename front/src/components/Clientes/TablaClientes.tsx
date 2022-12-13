8f1f00cfac158ac3fd1361557e0e7cb4556188a3
valentingr080 <valentingr080@gmail.com>
2022-12-13 16:56:16 +0100

    pal pesao de antonio

--- front/src/components/Clientes/TablaClientes.tsx
+++ front/src/components/Clientes/TablaClientes.tsx
@@ -35,7 +35,6 @@ function TablaClientes({clientes,clienteSeleccionado, setClienteSeleccionado}:pr
               <tr
                 key={key}
                 onClick={() => {
-                  cambiarVaribales(c.NIF, c.NOMBRE, c.APELLIDOS, c.EDAD)
                   setClienteSeleccionado({...c})}}
               >
                 <td>{c.NIF}</td>
